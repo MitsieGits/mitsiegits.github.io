95c1da522b148b6e0c2a2aedbc25b3f1a79f39d3
MitsieMits <michelwildhagen+github@gmail.com>
2023-11-17 12:59:50 +0100

    Added new button with 'go to top' function

--- script.js
+++ script.js
@@ -51,3 +51,10 @@ function toggleMenu() {
       icon.src = icon.getAttribute("src-light");
     });
   }
+
+  // Go to top button
+
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  } 
